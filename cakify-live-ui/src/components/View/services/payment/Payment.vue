4210b5fa3021a2bad137e9a299ef9cde905988e7
nikitha <nikitha.devan.23@gmail.com>
2020-11-02 23:01:32 -0600

    added payment

---
+++ cakify-live-ui/src/components/View/services/payment/Payment.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h3>Payment</h3>
+        <div class="box">
+            <p>You will be redirected to our payment portal. Please be ready with your bank details to complete your transaction.</p>
+            <p>Thank You for using Cakify!</p>
+            <v-card-actions>
+            <v-btn
+                color="deep-purple lighten-2"
+                text
+                @click="proceedToCheckout"
+            >
+                Proceed To Checkout
+            </v-btn>
+        </v-card-actions>
+        </div>
+        
+         
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                paymentUrl: ''
+            }
+        },
+        created(){
+            this.paymentUrl = this.$route.params.paymentUrl;
+        },
+        methods: {
+            proceedToCheckout(){
+                window.open(this.paymentUrl)
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ cakify-live-ui/src/components/View/services/payment/PaymentPage.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <!-- Wrapper -->
+    <div id="wrapper">
+        <!-- Main -->
+        <div id="main">
+            <div class="inner">
+                <!-- Header -->
+                <Header />
+                <!-- Header -->
+                <!-- Form -->
+                <Payment />
+
+                <!-- Form -->
+            </div>
+        </div>
+        <!-- Main ends -->
+
+    </div>
+</div>
+</template>
+
+<script>
+import Header from "@/components/View/common/Header";
+import Payment from "@/components/View/services/payment/Payment.vue";
+
+export default {
+    components: {
+        Header,
+        Payment
+    }
+};
+</script>
