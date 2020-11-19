d3ad6433d748aa737599c2aade644a6e055cfc2f
MayaJp <mayajayaprakash@gmail.com>
2020-11-18 22:54:57 -0600

    after login landing with only logged in bakery content

---
+++ cakify/src/components/View/services/search_cakes/CakifySearchCakePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- Wrapper -->
+    <div id="wrapper">
+      <!-- Main -->
+      <div id="main">
+        <div class="inner">
+          <!-- Header -->
+          <Header />
+          <!-- Header -->
+          <!-- Form -->
+          <CakifySearchCake />
+
+          <!-- Form -->
+        </div>
+      </div>
+      <!-- Main ends -->
+
+      <Sidebar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/View/common/Header";
+import Sidebar from "@/components/View/common/Sidebar";
+import CakifySearchCake from "@/components/View/services/search_cakes/CakifySearchCake.vue";
+
+export default {
+  components: {
+    Header,
+    Sidebar,
+    CakifySearchCake,
+  },
+};
+</script>
