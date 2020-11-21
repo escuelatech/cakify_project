584377cac4066683ddba81b36f3fc05314b22aa3
nikitha <nikitha.devan.23@gmail.com>
2020-11-21 09:53:51 -0600

    added dashboard

---
+++ cakify/src/components/View/services/LandingPage/Dashboard.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <h1>Dashboard</h1>
+        <div class="box alt">
+            <div class="row gtr-50 gtr-uniform">
+                <div class="col-4">
+                    <span class="image fit">
+                        <div class="box icon-container box1">
+                            <h3>Your Orders</h3>
+                            <div class="icon-container">
+                                <i class="fas fa-inbox fa-3x"></i>
+                            </div>
+                            <h1>10</h1>
+                            
+                        </div>
+                    </span>
+                </div>
+                
+                 <div class="col-4">
+                    <span class="image fit">
+                         <div class="box icon-container box2">
+                            <h3>Payment Received</h3>
+                            <div class="icon-container">
+                                <i class="fas fa-rupee-sign fa-3x"></i>
+                            </div>
+                            <h1>10000</h1>
+                            
+                        </div>
+                    </span>
+                </div>
+                 <div class="col-4">
+                    <span class="image fit">
+                        <div class="box icon-container box3">
+                            <h3>Payment Received</h3>
+                            <div class="icon-container">
+                                <i class="fas fa-rupee-sign fa-3x"></i>
+                            </div>
+                            <h1>10000</h1>
+                            
+                        </div>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.icon-container{
+    text-align: center;
+    color: white;
+}
+.box1{
+    background-color: DarkCyan;
+}
+.box2{
+    background-color: IndianRed;
+}
+.box3{
+    background-color: DodgerBlue;
+}
+h3, h1{
+    color: white;
+}
+h3{
+    border-bottom: 5px solid white;
+}
+
+
+</style>
