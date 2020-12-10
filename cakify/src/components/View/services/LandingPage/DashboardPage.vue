d35864987b844e3b6782dea9e7056da6711d228d
nikitha723 <46805620+nikitha723@users.noreply.github.com>
2020-12-09 23:10:49 -0600

    added dashboard (#16)
    Co-authored-by: nikitha <nikitha.devan.23@gmail.com>

---
+++ cakify/src/components/View/services/LandingPage/DashboardPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- Wrapper -->
+    <div id="wrapper">
+      <!-- Main -->
+      <div id="main">
+        <div class="inner">
+          <!-- Header -->
+          <Header />
+          <!-- Header -->
+          <!-- Form -->
+          <Dashboard />
+          <!-- Form -->
+        </div>
+      </div>
+      <!-- Main ends -->
+
+      <!-- Sidebar -->
+      <Sidebar />
+      <!-- Sidebar -->
+    </div>
+  </div>
+</template>
+<script>
+  import Header from "@/components/View/common/Header";
+  import Sidebar from "@/components/View/common/Sidebar";
+  import Dashboard from "@/components/View/services/LandingPage/Dashboard.vue";
+
+  export default {
+      components: { 
+          Header, 
+          Sidebar, 
+          Dashboard
+        }
+    };
+</script> 
