1e49240ce3e7fc4fe66e475f8ee50617e4528d35
nikitha723 <46805620+nikitha723@users.noreply.github.com>
2020-12-16 08:45:46 -0600

    Displaying a list of cake orders and displaying all the order details by orderID in another component (#23)
    Co-authored-by: nikitha <nikitha.devan.23@gmail.com>

---
+++ cakify/src/components/View/services/order_details/IndividualOrderDetailPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- Wrapper -->
+    <div id="wrapper">
+      <!-- Main -->
+      <div id="main">
+        <div class="inner">
+          <!-- Header -->
+          <Header />
+          <!-- Header -->
+          <!-- Form -->
+          <IndividualOrderDetail />
+          <!-- Form -->
+        </div>
+      </div>
+      <!-- Main ends -->
+
+      <!-- Sidebar -->
+      <Sidebar />
+      <!-- Sidebar -->
+    </div>
+  </div>
+</template>
+<script>
+  import Header from "@/components/View/common/Header";
+  import Sidebar from "@/components/View/common/Sidebar";
+  import IndividualOrderDetail from "@/components/View/services/order_details/IndividualOrderDetail.vue";
+
+  export default {
+      components: { 
+          Header, 
+          Sidebar, 
+          IndividualOrderDetail
+        }
+    };
+</script> 
